e1c34f8c25766d83f9d714808dfd92631abf3a83
apoorvanand <apoorwanand@gmail.com>
2019-01-05 14:18:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 /*jshint esversion: 6 */
 
 'use strict';
+var metrics = require('datadog-metrics');
+metrics.init({ host: 'myhost', prefix: 'myapp.' });
+ 
+function collectMemoryStats() {
+    var memUsage = process.memoryUsage();
+    metrics.gauge('memory.rss', memUsage.rss);
+    metrics.gauge('memory.heapTotal', memUsage.heapTotal);
+    metrics.gauge('memory.heapUsed', memUsage.heapUsed);
+};
+ 
+setInterval(collectMemoryStats, 2000);
 const tracer = require('dd-trace').init()
 
 const Raven = require('raven');
